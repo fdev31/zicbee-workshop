a24f407a5c6bb1c9a0cf2471f825058f741ec11d
fab <fdev31@gmail.com>
2009-10-09 23:54:18 +0200

    add underscore in front of hive_upgrade_script.sh
    --HG--
    rename : environment/hive_upgrade_script.sh => environment/_hive_upgrade_script.sh

--- environment/hives_builder.sh
+++ environment/hives_builder.sh
@@ -48,7 +48,7 @@ for pyversion in $PY_VERSIONS; do
     fi
 
     cd $env_path || die "cd $env_path"
-    cp ../hive_upgrade_script.sh ./UPGRADE
+    cp ../_hive_upgrade_script.sh ./UPGRADE
     cp ../_run_tests.sh ./run_tests.sh
     ./UPGRADE $SRC || die "upgrade failed"
 
